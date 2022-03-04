e3db8b3f6368fd8a068ade8ef20f1d69380f5686
Joseph Hernandez <89801469+JosephHernandez26@users.noreply.github.com>
2022-03-03 17:18:31 -0800

    completes priority queue quick code practice.

--- Udemy/Colts-Class/Data-Structures/Heaps/heaps.js
+++ Udemy/Colts-Class/Data-Structures/Heaps/heaps.js
@@ -120,3 +120,81 @@ class MaxBinaryHeap2 { // Define the class
   //   }
   //    return current;
   // }
+//+++++++++++++++++++++++++++++++++++++++++
+// ******* The Priority Queue ***********
+//+++++++++++++++++++++++++++++++++++++++++
+
+// triage based on priority level basically (think the ER but for data)
+
+// ** Creating a Min Heap **
+// ** Using Enqueue and Dequeue **
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue (value, priority) {
+    let newNode = new Node (value, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let index = this.values.length -1;
+    const element = this.values[index];
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while(true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let rightChild, leftChild;
+      let swap = null;
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild.priority > element.priority) {
+          swap = leftChildIndex;
+        }
+      }
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !==null && rightChild.priority < leftChild.priority)
+          ){
+            swap = rightChildIndex;
+        }
+        if (swap === null) break;
+        this.values[index] = this.values[swap];
+        this.values[swap] = element;
+        index = swap;
+      }
+    }
+  }
+}
+
+class Node {
+  constructor(value, priority) {
+    this.value = value;
+    this.priority = priority;
+  }
+
+}
