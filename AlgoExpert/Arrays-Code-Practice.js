4f19effa7a38bd3424221a3b04b7aaa5c82a5127
Joseph Hernandez <89801469+JosephHernandez26@users.noreply.github.com>
2022-11-03 11:36:26 -0700

    completed daily practice

--- AlgoExpert/Arrays-Code-Practice.js
+++ AlgoExpert/Arrays-Code-Practice.js
@@ -3,3 +3,14 @@
 /// Algo Expert Arrays Practice
 
 // Two-Number Sum
+const twoNumSum = (arr, targetSum) => {
+  const numsTable = {};
+  for (let num in arr) {
+    let targetNum = targetSum - num;
+    if (targetNum in numsTable) return [targetNum, num];
+    else targetNum[num] = true;
+  }
+  return [];
+};
+
+//
